86f82a90fe39300af632e8d618117fae2a76b0df
m4rqu1t <marcotab422@gmail.com>
2026-03-30 11:18:26 +0000

    DTO e service da historia-3

---
+++ backend/sebrae/src/main/java/io/polaris/sebrae/dto/InactivityRequestDTO.java
@@ -0,0 +1,34 @@
+package io.polaris.sebrae.dto;
+
+public class InactivityRequestDTO {
+
+    private Long userId;
+    private Long lessonId;
+    private Long courseId;
+
+    public InactivityRequestDTO() {}
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getLessonId() {
+        return lessonId;
+    }
+
+    public void setLessonId(Long lessonId) {
+        this.lessonId = lessonId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+}
